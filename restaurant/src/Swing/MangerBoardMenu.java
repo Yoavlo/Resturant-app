29001726eddab8a406aa265726e2cf64dee6b35e
yoavlo <yoavlongman@gmail.com>
2018-04-25 01:03:19 +0300

    update MangerBoardMenu to be WindowBuilder

--- restaurant/src/Swing/MangerBoardMenu.java
+++ restaurant/src/Swing/MangerBoardMenu.java
@@ -78,7 +78,7 @@ public class MangerBoardMenu {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		;
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
